ca23e1f325aff882c824b161c784e49fc3409194
grmanjula <63128282+grmanjula@users.noreply.github.com>
2020-10-29 23:52:30 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoritmsandDataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
